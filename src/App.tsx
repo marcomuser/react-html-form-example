9a0346a83fc22311900cd9a6b99eb8f168cb4622
Marco Muser <marco.muser@enpal.de>
2024-12-08 12:40:31 +0100

    refactor example

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { useForm } from "react-hook-form";
 import "./App.css";
 
 interface FormValues {
-  firstName: string;
-  gender: "female" | "male" | "other";
+  temperature: string;
+  country: "sweden" | "india";
 }
 
 export default function App() {
@@ -15,30 +15,34 @@ export default function App() {
   return (
     <form onSubmit={handleSubmit((data) => console.log(data))} className="form">
       <label>
-        First Name
+        Country
+        <select {...register("country", { required: true })}>
+          <option value=""></option>
+          <option value="sweden">Sweden</option>
+          <option value="india">India</option>
+        </select>
+      </label>
+
+      <label>
+        Temperature
         <input
           type="number"
-          {...register("firstName", {
-            required: "required",
-            min: -22,
+          {...register("temperature", {
+            required: true,
+            min: -30,
+            max: 60,
             validate: {
-              malePositive: (v, formValues) =>
-                formValues.gender === "male" && parseInt(v) < 0
-                  ? "not valid"
+              coldEurope: (v, formValues) =>
+                formValues.country === "sweden" && parseInt(v) > 40
+                  ? "Not valid. It doesn't get this hot in Sweden!"
+                  : true,
+              hotIndia: (v, formValues) =>
+                formValues.country === "india" && parseInt(v) < 0
+                  ? "Not valid. It doesn't get this cold in India!"
                   : true,
             },
           })}
         />
-      </label>
-
-      <label>
-        Gender Selection
-        <select {...register("gender", { required: "required" })}>
-          <option value=""></option>
-          <option value="female">female</option>
-          <option value="male">male</option>
-          <option value="other">other</option>
-        </select>
       </label>
 
       <button>Submit</button>
